91f28142b4e4c0250eb8d8be248cf47d47bda231
xilele777 <2519175810@qq.com>
2026-08-04 20:45:18 +0800

    Remove test and scaffold example articles, simplify nav

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -23,8 +23,7 @@ const vitePressOptions = {
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '首页', link: '/' }
     ],
 
     socialLinks: [
